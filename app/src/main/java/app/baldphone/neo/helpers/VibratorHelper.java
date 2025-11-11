DamianKuzmiak/BaldPhone

Refactor BaldImageButton.java

app/src/main/java/app/baldphone/neo/helper/VibratorHelper.java
@@ -1,0 +1,118 @@
+package app.baldphone.neo.helper;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.os.VibratorManager;
+import android.util.Log;
+
+import androidx.annotation.IntRange;
+import androidx.annotation.NonNull;
+
+/**
+ * This class provides a simplified interface to the Android {@link Vibrator} service. Implemented
+ * as a singleton to ensure a single point of control for vibrations.
+ */
+public class VibratorHelper {
+
+    private static final String TAG = "VibratorHelper";
+    private static volatile VibratorHelper sInstance;
+
+    private final Vibrator vibratorService;
+    private final boolean hasVibratorHardware;
+
+    private VibratorHelper(Context context) {
+        this.vibratorService = getVibratorService(context);
+
+        if (this.vibratorService == null) {
+            Log.e(TAG, "Vibrator service could not be retrieved.");
+            this.hasVibratorHardware = false;
+        } else {
+            this.hasVibratorHardware = this.vibratorService.hasVibrator();
+            if (!this.hasVibratorHardware) {
+                Log.w(TAG, "Device does not have vibrator hardware.");
+            }
+        }
+    }
+
+    /**
+     * Initializes the VibratorHelper singleton. Should be called once, preferably in the
+     * Application's onCreate method.
+     *
+     * @param context Application context is preferred.
+     */
+    public static void init(@NonNull Context context) {
+        if (sInstance == null) {
+            synchronized (VibratorHelper.class) {
+                if (sInstance == null) {
+                    sInstance = new VibratorHelper(context);
+                }
+            }
+        }
+    }
+
+    /**
+     * Performs a one-shot vibration for the specified duration. Call VibratorHelper.init(context)
+     * once before using this method.
+     *
+     * @param durationMs Duration of the vibration in milliseconds. Must be positive.
+     */
+    public static void vibrate(long durationMs) {
+        VibratorHelper instance = getInstance();
+        if (!instance.canVibrateInternal()) {
+            return;
+        }
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                VibrationEffect effect =
+                        VibrationEffect.createOneShot(
+                                durationMs, VibrationEffect.DEFAULT_AMPLITUDE);
+                instance.vibratorService.vibrate(effect);
+            } else {
+                instance.vibratorService.vibrate(durationMs);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error during vibration", e);
+        }
+    }
+
+    /**
+     * Cancels any ongoing vibration. Call VibratorHelper.init(context) once before using this
+     * method.
+     */
+    public static void cancel() {
+        VibratorHelper instance = getInstance();
+        if (instance.vibratorService != null) {
+            try {
+                instance.vibratorService.cancel();
+            } catch (Exception e) {
+                Log.e(TAG, "Error cancelling vibration", e);
+            }
+        }
+    }
+
+    private static VibratorHelper getInstance() {
+        if (sInstance == null) {
+            throw new IllegalStateException(
+                    "VibratorHelper not initialized. Call VibratorHelper.init(context)");
+        }
+        return sInstance;
+    }
+
+    private Vibrator getVibratorService(Context context) {
+        Context appContext = context.getApplicationContext();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            VibratorManager vibratorManager =
+                    (VibratorManager) appContext.getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
+            return vibratorManager != null ? vibratorManager.getDefaultVibrator() : null;
+        } else {
+            return (Vibrator) appContext.getSystemService(Context.VIBRATOR_SERVICE);
+        }
+    }
+
+    private boolean canVibrateInternal() {
+        return this.vibratorService != null && this.hasVibratorHardware;
+    }
+}

app/src/main/java/com/bald/uriah/baldphone/BaldPhone.java
@@ -30,6 +30,8 @@ import com.bald.uriah.baldphone.utils.S;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
+import app.baldphone.neo.helper.VibratorHelper;
+
 public class BaldPhone extends Application {
     private static final String TAG = BaldPhone.class.getSimpleName();
     // Application class should not have any fields, http://www.developerphil.com/dont-store-data-in-the-application-object/
@@ -50,6 +52,7 @@ public class BaldPhone extends Application {
             Log.e(TAG, e.getMessage());
             e.printStackTrace();
         }
+        VibratorHelper.init(this);
         S.sendVersionInfo(this);
     }
 

DamianKuzmiak/BaldPhoneNeo

app/src/main/java/app/baldphone/neo/helpers/VibratorHelper.java
@@ -1,0 +1,117 @@
+package app.baldphone.neo.helpers;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.os.VibratorManager;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+/**
+ * This class provides a simplified interface to the Android {@link Vibrator} service. Implemented
+ * as a singleton to ensure a single point of control for vibrations.
+ */
+public class VibratorHelper {
+
+    private static final String TAG = "VibratorHelper";
+    private static volatile VibratorHelper sInstance;
+
+    private final Vibrator vibratorService;
+    private final boolean hasVibratorHardware;
+
+    private VibratorHelper(Context context) {
+        this.vibratorService = getVibratorService(context);
+
+        if (this.vibratorService == null) {
+            Log.e(TAG, "Vibrator service could not be retrieved.");
+            this.hasVibratorHardware = false;
+        } else {
+            this.hasVibratorHardware = this.vibratorService.hasVibrator();
+            if (!this.hasVibratorHardware) {
+                Log.w(TAG, "Device does not have vibrator hardware.");
+            }
+        }
+    }
+
+    /**
+     * Initializes the VibratorHelper singleton. Should be called once, preferably in the
+     * Application's onCreate method.
+     *
+     * @param context Application context is preferred.
+     */
+    public static void init(@NonNull Context context) {
+        if (sInstance == null) {
+            synchronized (VibratorHelper.class) {
+                if (sInstance == null) {
+                    sInstance = new VibratorHelper(context);
+                }
+            }
+        }
+    }
+
+    /**
+     * Performs a one-shot vibration for the specified duration. Call VibratorHelper.init(context)
+     * once before using this method.
+     *
+     * @param durationMs Duration of the vibration in milliseconds. Must be positive.
+     */
+    public static void vibrate(long durationMs) {
+        VibratorHelper instance = getInstance();
+        if (!instance.canVibrateInternal()) {
+            return;
+        }
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                VibrationEffect effect =
+                        VibrationEffect.createOneShot(
+                                durationMs, VibrationEffect.DEFAULT_AMPLITUDE);
+                instance.vibratorService.vibrate(effect);
+            } else {
+                instance.vibratorService.vibrate(durationMs);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error during vibration", e);
+        }
+    }
+
+    /**
+     * Cancels any ongoing vibration. Call VibratorHelper.init(context) once before using this
+     * method.
+     */
+    public static void cancel() {
+        VibratorHelper instance = getInstance();
+        if (instance.vibratorService != null) {
+            try {
+                instance.vibratorService.cancel();
+            } catch (Exception e) {
+                Log.e(TAG, "Error cancelling vibration", e);
+            }
+        }
+    }
+
+    private static VibratorHelper getInstance() {
+        if (sInstance == null) {
+            throw new IllegalStateException(
+                    "VibratorHelper not initialized. Call VibratorHelper.init(context)");
+        }
+        return sInstance;
+    }
+
+    private Vibrator getVibratorService(Context context) {
+        Context appContext = context.getApplicationContext();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            VibratorManager vibratorManager =
+                    (VibratorManager) appContext.getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
+            return vibratorManager != null ? vibratorManager.getDefaultVibrator() : null;
+        } else {
+            return (Vibrator) appContext.getSystemService(Context.VIBRATOR_SERVICE);
+        }
+    }
+
+    private boolean canVibrateInternal() {
+        return this.vibratorService != null && this.hasVibratorHardware;
+    }
+}

app/src/main/java/com/bald/uriah/baldphone/BaldPhone.java
@@ -21,6 +21,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import app.baldphone.neo.helpers.VibratorHelper;
+
 import com.bald.uriah.baldphone.activities.UpdatesActivity;
 import com.bald.uriah.baldphone.databases.alarms.AlarmScheduler;
 import com.bald.uriah.baldphone.databases.reminders.ReminderScheduler;
@@ -50,6 +52,7 @@ public class BaldPhone extends Application {
             Log.e(TAG, e.getMessage());
             e.printStackTrace();
         }
+        VibratorHelper.init(this);
         S.sendVersionInfo(this);
     }
 